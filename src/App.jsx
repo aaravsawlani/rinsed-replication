7e18b49e6b54bc6da9185e18fce7a60932abe8fb
aaravsawlani <as14324@nyu.edu>
2025-01-31 12:59:05 -0500

    routing 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import TodayDashboard from "views/admin/default/Today.jsx";
 import CreateSegment from "views/admin/default/marketing/CreateSegment.jsx";
 import ViewAllSegments from "views/admin/default/marketing/ViewAllSegments.jsx";
@@ -13,15 +13,15 @@ const App = () => {
   return (
     <Router>
       <Sidebar />
-      <Routes>
-        <Route path="auth/*" element={<AuthLayout />} />
-        <Route path="admin/*" element={<AdminLayout />} />
-        <Route path="/" element={<Navigate to="/admin/default" replace />} />
-        <Route path="/admin/default/today" element={<TodayDashboard />} />
-        <Route path="/admin/marketing/create-segment" element={<CreateSegment />} />
-        <Route path="/admin/marketing/view-all-segments" element={<ViewAllSegments />} />
-        <Route path="/admin/marketing/create-message-campaign" element={<CreateMessageCampaign />} />
-      </Routes>
+      <Switch>
+        <Route path="/auth" component={AuthLayout} />
+        <Route path="/admin" component={AdminLayout} />
+        <Route path="/admin/default/today" component={TodayDashboard} />
+        <Route path="/admin/marketing/create-segment" component={CreateSegment} />
+        <Route path="/admin/marketing/view-all-segments" component={ViewAllSegments} />
+        <Route path="/admin/marketing/create-message-campaign" component={CreateMessageCampaign} />
+        <Redirect from="/" to="/admin/default/today" />
+      </Switch>
     </Router>
   );
 };
